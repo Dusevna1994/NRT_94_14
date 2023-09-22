0361aa3411243dd24b0a866432c0502c4908e05b
nikola <nikola94bgd@gmail.com>
2023-09-22 05:56:26 +0200

    prebacivanje oglasi .xml .dtd .xsd u folder OglasiServis

--- OglasiServis/index.js
+++ OglasiServis/index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const app = express();
+const port = 4000;
 const oglasiServis = require('radsaoglasimamodul');
 
-app.listen(4000,()=>{
-    console.log('server radi!')
+
+app.use(express.urlencoded({extended:false})); //parseri -> sluze da iz tela stranice objekte koji se salju serveru konvertuju u json objekat
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.send('Server radi\nPocetna str.')
+});
+
+
+
+app.listen(port,()=>{
+    console.log('Server AKTIVAN\nPORT: '+port);
 });
